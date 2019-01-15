d16c551c94113a9d3c5b77bed30faa5ba0f1191a
Donny Techiera <34099180+DonnyTech20@users.noreply.github.com>
2019-01-15 10:35:26 -0500

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  8 ball Magic Fun
+//
+//  Created by DonnyTech on 12/28/18.
+//  Copyright © 2018 Weeks Inspired. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    var randomBallNumber = 0
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        newBallImage()
+        
+      
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+
+    
+
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        randomBallNumber = Int(arc4random_uniform(5))
+        
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+}
+
